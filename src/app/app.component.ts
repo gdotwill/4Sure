035c125af585618f99dc8ccea29da7db05408c0e
Godwill <godwillmark@yahoo.co.uk>
2020-04-30 08:01:08 +0200

    4Sure project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Query } from '@syncfusion/ej2-data';
+import { EmitType } from '@syncfusion/ej2-base';
+import { FilteringEventArgs } from '@syncfusion/ej2-dropdowns';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'ForSure';
+    public data: { [key: string]: Object; }[] = [
+      { id: 1, name: 'Australia'},
+      { id: 2, name: 'Benin'},
+      { id: 3, name: 'Canada'},
+      { id: 4, name: 'Denmark'},
+      { id: 5, name: 'England'},
+      { id: 6, name: 'Finland'},
+      { id: 7, name: 'Granada'},
+      { id: 8, name: 'Honduras'},
+    ];
+
+    public fields :Object = { dataSource: this.data, value: 'id', text: 'name'};
+    public height: string = '250px';
+    public watermark: string = 'Select a country';
+    public filterPlaceholder: string = 'Search countries ...';
+    public onFiltering: EmitType<FilteringEventArgs> = (e: FilteringEventArgs) => {
+        let query: Query = new Query();
+        query = (e.text !== '') ? query.where('name', 'startswith', e.text, true) : query;
+        e.updateData(this.data, query);
+    }
+
+    public treeSettings: Object = { autoCheck: true }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
+import { DropDownTreeModule } from '@syncfusion/ej2-angular-dropdowns';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations:
+  [ AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports:
+  [
+    ButtonModule,
+    BrowserModule,
+    FormsModule,
+    DropDownListModule,
+    DropDownTreeModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
